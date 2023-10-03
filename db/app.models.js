dcc7f2475b7af0857c39cbc4ce82329d7d6720f3
Meron G <merong@Merons-MacBook-Air.local>
2023-10-03 16:59:47 +0100

    implemented a patch votes endpoint

--- db/app.models.js
+++ db/app.models.js
@@ -83,3 +83,14 @@ exports.selectUsers = () => {
     })
     
     }
+
+exports.updateArticleVotesById = (article_id, inc_votes) => {
+return db.query(`
+    UPDATE articles
+    SET votes = votes + $1
+    WHERE article_id = $2
+    RETURNING *
+    `, [inc_votes, article_id]).then(({rows}) => {
+        return rows[0]
+    })
+    }
