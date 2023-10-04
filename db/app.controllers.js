07bf2b008cac802fa824811923f3ac4d5e4ef227
Meron G <merong@Merons-MacBook-Air.local>
2023-10-04 11:17:34 +0100

    final edit to delete comment

--- db/app.controllers.js
+++ db/app.controllers.js
@@ -104,13 +104,10 @@ exports.postCommentByArticleId =(req, res, next) => {
 exports.deleteCommentById = (req, res , next) => {
     const { comment_id } = req.params
 
-    selectCommentById(comment_id).then(() => {
-          removeCommentById(comment_id).then(() => {
-       res.status(204).send()
-    }).catch((err) => {
-       next(err)
-    })  
-    }).catch((err) => {
+    removeCommentById(comment_id).then(() => {
+    res.status(204).send()
+    })
+    .catch((err) => {
         next(err)
     })
 
