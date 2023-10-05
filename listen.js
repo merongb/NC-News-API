e0d62d9e4d00c26cc24706e577754442f7b0c165
Meron G <merong@Merons-MacBook-Air.local>
2023-10-05 10:03:07 +0100

    changes to app

--- listen.js
+++ listen.js
@@ -2,7 +2,7 @@ const app = require("./app")
 const { PORT = 9090 } = process.env
 
 app.listen(PORT, (err) => {if (err) {
-    console.log(err)
+    throw err
 }   else
  console.log(`Listening on ${PORT}...`)}
 )
